ff0d78d88a5d0c920d532e5e1b2004e93d9cb609
haroldCoder <haroldc2005@gmail.com>
2023-11-06 13:05:02 -0500

    add animation

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -48,8 +48,8 @@ export default function Info() {
   return (
     <div className='px-72 py-32'>
       <form action="" className="flex flex-col gap-10" onSubmit={(e)=>{e.preventDefault(), sendEmail()}}>
-        <input type="text" className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Full Name' onChange={(e)=>{setData((prev)=>({...prev, name: e.target.value}))}} value={data.name} required />
-        <select onChange={(e)=>{setData((prev)=>({...prev, country: e.target.value}))}} className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' required>
+        <input type="text" className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Full Name' onChange={(e)=>{setData((prev)=>({...prev, name: e.target.value}))}} value={data.name} required />
+        <select onChange={(e)=>{setData((prev)=>({...prev, country: e.target.value}))}} className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' required>
           <option>Country</option>
           {
             countries && countries.map((e)=>(
@@ -57,11 +57,11 @@ export default function Info() {
             ))
           }
         </select>
-        <input type="text" className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='City' onChange={(e)=>{setData((prev)=>({...prev, city: e.target.value}))}} value={data.city}  required />
-        <input type="text" className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Adress include(neighborhood)' onChange={(e)=>{setData((prev)=>({...prev, adress: e.target.value}))}} value={data.adress} required />
-        <input type="email" className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Email' onChange={(e)=>{setData((prev)=>({...prev, email: e.target.value}))}} value={data.email} required />
-        <input type="tel" className='bg-gray-600 w-[80%] rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Cell Phone' onChange={(e)=>{setData((prev)=>({...prev, phone: e.target.value}))}} value={data.phone} required />
-        <button className="bg-gray-600 font-semibold w-[80%] rounded-lg border-[1.5px] border-yellow-500 p-3 text-white">Submit</button>
+        <input type="text" className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='City' onChange={(e)=>{setData((prev)=>({...prev, city: e.target.value}))}} value={data.city}  required />
+        <input type="text" className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Adress include(neighborhood)' onChange={(e)=>{setData((prev)=>({...prev, adress: e.target.value}))}} value={data.adress} required />
+        <input type="email" className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Email' onChange={(e)=>{setData((prev)=>({...prev, email: e.target.value}))}} value={data.email} required />
+        <input type="tel" className='bg-gray-600 w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-md border-b-[1.5px] border-yellow-500 p-3 text-white' placeholder='Cell Phone' onChange={(e)=>{setData((prev)=>({...prev, phone: e.target.value}))}} value={data.phone} required />
+        <button className="bg-gray-600 font-semibold w-[80%] hover:animate-pulse animate-ease-in animate-alternate-reverse animate-fill-backwards rounded-lg border-[1.5px] border-yellow-500 p-3 text-white">Submit</button>
       </form>
     </div>
   )
